2147097f445b6cb2fc1e03164578475297e2b94e
Kaminishikawahara KK <kkaori146@gmail.com>
2022-01-06 11:44:04 +0900

    Design

--- luckyGame.py
+++ luckyGame.py
@@ -1,10 +1,15 @@
-import random
+from random import randint
+from time import sleep
 
-pc = random.randint(0, 5)
+pc = randint(0, 5)
+print('/=\=/'* 7)
 user = int(input('Inform a number: '))
+print('PROCESSING....')
 if (pc == user):
     print('\nYou won!')
     print(f'The number chosen by the pc was {pc}')
+    print('/=\=/' * 7)
 else:
     print('\nTry again!')
     print(f'The number chosen by the pc was {pc}')
+    print('/=\=/' * 7)
